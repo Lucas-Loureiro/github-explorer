aa8e8b0280f57a15a1a0bf48e23c4066ad43ec49
Lucas <loureiro4778@gmail.com>
2021-06-21 14:20:09 -0300

    Projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { useState } from 'react';
-import api from './services/api';
-
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import List from './pages/List'
+import Repository from './pages/Repository'
+import GlobalStyle from './styles/global'
 
 function App() {
- 
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={List} />
+        <Route path="/repository/:repository+" component={Repository} />
+      </Switch>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
 
 }
 
